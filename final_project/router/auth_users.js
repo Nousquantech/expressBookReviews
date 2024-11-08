57975bd7078a598986f501ca339e1600ffd85293
nousquantech <nousquantech@gmail.com>
2024-11-08 15:16:35 -0500

    task8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -18,12 +18,11 @@ const isValid = (username)=>{
     }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-    // Filter the users array for any user with the same username and password
+const authenticatedUser = (username,password)=>{ 
     let validusers = users.filter((user) => {
         return (user.username === username && user.password === password);
     });
-    // Return true if any valid user is found, otherwise false
+    // // Return true if any valid user is found, otherwise false
     if (validusers.length > 0) {
         return true;
     } else {
@@ -33,13 +32,11 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  const username = req.body.username;
-    const password = req.body.password;
-    // Check if username or password is missing
+  const username = req.query.username;
+    const password = req.query.password;
     if (!username || !password) {
         return res.status(404).json({ message: "Error logging in" });
     }
-    // Authenticate user
     if (authenticatedUser(username, password)) {
         // Generate JWT access token
         let accessToken = jwt.sign({
@@ -59,20 +56,21 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
 // Extract email parameter from request URL
     const isbn = req.params.isbn;
-    const username = req.body.username;
-    const review = req.body.review;
+    const username = req.query.username;
+    const review = req.query.review;
 
-    let book = books[isbn];  // Retrieve friend object associated with email
-    if (book) {  // Check if friend exists
-        if (username && review) {
-            book["reviews"]["username"]["review"] = review;
-            books[isbn] = book
-        }
-        res.send(`Review for book with isbn ${isbn} updated.`);
-    } else {
-        // Respond if friend with specified email is not found
-        res.send("Unable to add review!");
-    }
+    console.log(isbn,username,review)
+    // let book = books[isbn];  
+    // if (book) {  
+    //     if (username && review) {
+    //         book["reviews"]["username"]["review"] = review;
+    //         books[isbn] = book
+    //     }
+    //     res.send(`Review for book with isbn ${isbn} updated.`);
+    // } else {
+       
+    //     res.send("Unable to add review!");
+    // }
 });
 regd_users.delete("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
